fdc32f23432e313ca9c986da589dcbb3b224e66c
aniketvasadkar <aniketvasadkarofficial@gmail.com>
2019-11-14 23:57:12 +0530

    Adding eureka server

---
+++ eureka-server/src/main/java/com/eureka/server/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.eureka.server.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+
+}
