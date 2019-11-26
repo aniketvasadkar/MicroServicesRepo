8c556a770bc775db01045fee1a47819ae2d849e2
aniketvasadkar <aniketvasadkarofficial@gmail.com>
2019-11-27 00:34:58 +0530

    Added changes related to feign client

--- MicroService1/src/main/java/com/micro/service/MicroService1/MicroService1Application.java
+++ MicroService1/src/main/java/com/micro/service/MicroService1/MicroService1Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.micro.service.MicroService1"})
 @EnableJpaRepositories("com.micro.service.MicroService1.repo")
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MicroService1Application {
 
 	public static void main(String[] args) {
