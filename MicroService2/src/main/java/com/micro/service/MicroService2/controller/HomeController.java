bc368420905186d85535ecace98ec44b5f1c1aad
aniketvasadkar <aniketvasadkarofficial@gmail.com>
2019-11-27 23:38:35 +0530

    sleuth distributed logging implemented

--- MicroService2/src/main/java/com/micro/service/MicroService2/controller/HomeController.java
+++ MicroService2/src/main/java/com/micro/service/MicroService2/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.micro.service.MicroService2.controller;
 
 import com.micro.service.MicroService2.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,14 @@ import java.util.List;
 @RestController
 public class HomeController {
 
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     TestService testService;
 
     @RequestMapping(value = "home", method = RequestMethod.GET,produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<String> getHomeResponse(){
-        System.out.println("---------------Greeting from microservice 2---------------------------");
+        log.info("---------------Greeting from microservice 2---------------------------");
         List<String> greet = new ArrayList<String>();
         greet.add(testService.testHystrix());
         return greet;
